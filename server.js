47f932eaccf35bc074e050abc931f5dbec80f0f3
Scipio Africanus <jljones657@gmail.com>
2018-05-11 23:50:15 -0500

    well it works on my machine right now

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-//Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+var db = require("./models")
 
-// Sets up the Express app to handle data parsing
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
 app.use(bodyParser.json());
 
-//Set Handlebars
-var exphbs = require("express-handlebars");
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-//Set routes so the server has access to them
-var routes = require("./controllers/burgers_controller.js");
-
-app.use(routes);
-
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
+});
